9b44186ee828ea6d086d43b66ad38fc166bb2723
Aa5hut0sh <aashutoshsharma2905@gmail.com>
2026-01-28 12:09:47 +0530

    Updated client file

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-center min-h-screen items-center text-8xl font-bold'>
+      GFG website
+    </div>
+  )
+}
+
+export default App
--- client/src/services/eventservice.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const eventservice = () => {
-  return (
-    <div>eventservice</div>
-  )
-}
-
-export default eventservice
