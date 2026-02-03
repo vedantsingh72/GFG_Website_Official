6dd055a3a605f9a82b970bbfd4bd33ae8cfcd8e4
Vedant Singh <vedantsingh0072@gmail.com>
2026-02-04 01:50:29 +0530

    aaaaaA

--- client/src/pages/admin.tsx
+++ client/src/pages/admin.tsx
@@ -1,9 +1,23 @@
+import EventForm from "../components/eventform";
 
-
-function admin() {
+function Admin() {
   return (
-    <div>admin</div>
-  )
+    <div
+      className="
+        min-h-screen w-full
+        flex items-center justify-center
+        bg-gradient-to-br from-black via-gray-900 to-black
+        relative overflow-hidden
+      "
+    >
+      <div className="absolute w-[600px] h-[600px] bg-green-500/20 blur-[120px] rounded-full top-[-150px] left-[-150px]" />
+      <div className="absolute w-[600px] h-[600px] bg-green-500/10 blur-[120px] rounded-full bottom-[-150px] right-[-150px]" />
+
+      <div className="relative z-10">
+        <EventForm />
+      </div>
+    </div>
+  );
 }
 
-export default admin
+export default Admin;
