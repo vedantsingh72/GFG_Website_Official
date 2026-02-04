1f16efaef124d5f50b29e48196f29e6568b98d79
Aa5hut0sh <aashutoshsharma2905@gmail.com>
2026-02-04 21:32:18 +0530

    Home animation fix

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,17 +1,18 @@
-
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 25s linear infinite",
+      },
       keyframes: {
         "loop-scroll": {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-100%)" }, 
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-50%)" }, // Use -50% for duplicated lists
         },
-      },
-      animation: {
-        "loop-scroll": "loop-scroll 30s linear infinite",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")], 
+  plugins: [require("tailwindcss-animate")],
 };
