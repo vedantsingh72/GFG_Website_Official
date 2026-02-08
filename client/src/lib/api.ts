5148fd8ac853f511c898b17cf1f5dd1ff9c4710a
Aa5hut0sh <aashutoshsharma2905@gmail.com>
2026-02-08 18:23:39 +0530

    mongodb added

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: `/api`,
+  baseURL: `/`,
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json'
